518001b3a7747798566ca4a2b95f6f14779453f5
s-brez <sam@sdbgroup.io>
2020-06-23 13:25:43 +1000

    IG auth working

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import requests
+from requests import Request, Session
 import json
 import os
 
 
-def lambda_handler(tradingview_webhook_signal):
+def lambda_handler(event, context):
     """
     Setup:
     Copy run() function body into AWS lambda and set required enviroment vars.
@@ -11,38 +11,69 @@ def lambda_handler(tradingview_webhook_signal):
 
     Event flow:
     1. Signal received from TradingView alert webhook.
-    2. Calculate entry/stop/TP order size.
-    3. Raise orders with venue.
+    2. Load relevant auth tokens from environment variables.
+    3. Parse incoming signal and calculate entry/stop/TP order size.
+    4. Raise orders with venue.
 
     """
 
-    # START LOAD AUTH TOKENS
-    if os.environ['IG_API_KEY'] and os.environ['IG_API_SECRET']:
-        IG_API_KEY = os.environ['IG_API_KEY']
-        IG_API_SECRET = os.environ['IG_API_SECRET']
+    # Set true for live trading.
+    LIVE = False
+
+    # Load auth tokens from environment variables.
+    if LIVE:
+        if(  # Live.
+            os.environ['IG_API_KEY'] and os.environ['IG_USERNAME'] and
+                os.environ['IG_PASSWORD']):
+            IG_API_KEY = os.environ['IG_API_KEY']
+            IG_USERNAME = os.environ['IG_USERNAME']
+            IG_PASSWORD = os.environ['IG_PASSWORD']
+            IG_URL = "https://api.ig.com/gateway/deal/session"
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps(
+                    "IG Markets live authentication tokens missing")}
     else:
-        raise Exception("IG Markets API keys missing.")
-    # END LOAD AUTH TOKENS
+        if(  # Demo.
+            os.environ['IG_API_KEY_DEMO'] and os.environ['IG_USERNAME_DEMO'] and
+                os.environ['IG_PASSWORD_DEMO']):
+            IG_API_KEY = os.environ['IG_API_KEY_DEMO']
+            IG_USERNAME = os.environ['IG_USERNAME_DEMO']
+            IG_PASSWORD = os.environ['IG_PASSWORD_DEMO']
+            IG_URL = "https://demo-api.ig.com/gateway/deal/session"
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps(
+                    "IG Markets demo authentication tokens missing")}
 
-    # START IG API CLIENT
-    # Instantiate an IG Markets python client, using the keys loaded above.
-    ig_client = None
-    # END IG API CLIENT
+    # Prepare request for IG.
+    headers = {
+        'X-IG-API-KEY': IG_API_KEY,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json; charset=UTF-8'}
 
-    # START PARSE WEBHOOK SIGNAL
-    # Add logic to parse incoming webhook signal here.
-    parsed_signal = None
-    # END PARSE WEBHOOK SIGNAL
+    body = {
+        "identifier": IG_USERNAME,
+        "password": IG_PASSWORD}
+
+    # Open new session with IG.
+    request = Request('POST', IG_URL, json=body,
+                      headers=headers, params='').prepare()
+
+    response = Session().send(request)
+
+    print(response)
+
+    # Parse the incoming webhook json.
+    tradingview_webhook_signal = json.loads(event['body'])
 
     # START ORDER SIZING & SUBMISSION
     # Add logic here to prepare prders to be sent to IG.
     orders_to_send = None
-    # Send prepared prders to IG with their provided client methods.
+    # Send prepared prders to IG with provided client methods.
     order_confirmations = None
-    # END ORDER SIZING & SUBMISSION
 
-    # START SEND USER NOTIFICATION (OPTIONAL)
-    # Add messaging client logic here (e.g Telegram or email) to notify user
-    # of signals/trades taken.
-    telegram_client = None
-    # END SEND USER NOTIFICATION
+
+lambda_handler({"body": '{"text": "Test"}'}, None)
