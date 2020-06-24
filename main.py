7eb6c6b2b29cf50669b8f99ccb33a6df3ef81f3e
s-brez <sam@sdbgroup.io>
2020-06-24 12:17:25 +1000

    add dynamic instrument detail fetch

--- main.py
+++ main.py
@@ -91,56 +91,55 @@ def lambda_handler(event, context):
             'X-SECURITY-TOKEN': XST,
             'CST': CST}
 
-        # Australian ticker code > IG epic mapping.
-        # Aus uses non expiring contracts. No need to fetch dynamically.
-        ticker_epic = {
-            "UKOIL": "CC.D.LCO.UMA.IP",
-            "CFDs on Brent Crude Oil": "CC.D.LCO.UMA.IP",
-            "DE30EUR": "IX.D.DAX.IFA.IP",
-            "DAX": "IX.D.DAX.IFA.IP",
-            "WHTUSD": "CC.D.W.UMA.IP",
-            "WHEATUSD": "CC.D.W.UMA.IP"}
+        brent_epic, brent_expiry, brent_lotsize = None, None, None
+        dax_epic, dax_expiry, dax_lotsize = None, None, None
+        wheat_epic, wheat_expiry, wheat_lotsize = None, None, None
 
-        # Get epics dynamically for UK markets as UK uses expiring contracts
-        brent_markets = Session().send(
-            Request('GET', IG_URL + "/markets?searchTerm=brent",
-                    headers=headers, params='').prepare())
-        brent_epic_uk = None
-        for market in brent_markets.json()['markets']:
-            if market['expiry'] != "DFB" and market['instrumentName'] == "Oil - Brent Crude" and market['instrumentType'] == "COMMODITIES":
-                brent_epic_uk = market["epic"]
-                break
+        # Fetch instrument details only if webhook signal has an appropriate ticker code.
+        if webhook_signal['ticker'] == "UKOIL" or webhook_signal['ticker'] == "CFDs on Brent Crude Oil":
+            brent_markets = Session().send(Request('GET', IG_URL + "/markets?searchTerm=brent", headers=headers, params='').prepare())
+            for market in brent_markets.json()['markets']:
+                if market['expiry'] != "DFB" and market['instrumentName'][:17] == "Oil - Brent Crude" and market['instrumentType'] == "COMMODITIES":
+                    brent_epic, brent_expiry = market["epic"], market["expiry"]
+                    break
+            brent_lotsize = Session().send(Request('GET', IG_URL + "/markets/" + brent_epic, headers=headers, params='').prepare()).json()['instrument']['lotSize']
 
-        dax_markets = Session().send(
-            Request('GET', IG_URL + "/markets?searchTerm=dax",
-                    headers=headers, params='').prepare())
-        dax_epic_uk = None
-        for market in dax_markets.json()['markets']:
-            if market['expiry'] != "DFB" and market['instrumentName'] == "Germany 30" and market['instrumentType'] == "INDICES":
-                dax_epic_uk = market["epic"]
-                break
+        elif webhook_signal['ticker'] == "DE30EUR" or webhook_signal['ticker'] == "DAX":
+            dax_markets = Session().send(Request('GET', IG_URL + "/markets?searchTerm=dax", headers=headers, params='').prepare())
+            for market in dax_markets.json()['markets']:
+                if market['expiry'] != "DFB" and market['instrumentName'][:10] == "Germany 30" and market['instrumentType'] == "INDICES":
+                    dax_epic, dax_expiry = market["epic"], market["expiry"]
+                    break
+            dax_lotsize = Session().send(Request('GET', IG_URL + "/markets/" + dax_epic, headers=headers, params='').prepare()).json()['instrument']['lotSize']
 
-        wheat_markets = Session().send(
-            Request('GET', IG_URL + "/markets?searchTerm=chicago%20wheat",
-                    headers=headers, params='').prepare())
-        wheat_epic_uk = None
-        for market in wheat_markets.json()['markets']:
-            if market['expiry'] != "DFB" and market['instrumentName'] == "Chicago Wheat" and market['instrumentType'] == "COMMODITIES":
-                wheat_epic_uk = market["epic"]
-                break
+        elif webhook_signal['ticker'] == "WHEATUSD" or webhook_signal['ticker'] == "WHTUSD":
+            wheat_markets = Session().send(Request('GET', IG_URL + "/markets?searchTerm=chicago%20wheat", headers=headers, params='').prepare())
+            for market in wheat_markets.json()['markets']:
+                if market['expiry'] != "DFB" and market['instrumentName'][:13] == "Chicago Wheat" and market['instrumentType'] == "COMMODITIES":
+                    wheat_epic, wheat_expiry = market["epic"], market["expiry"]
+                    break
+            wheat_lotsize = Session().send(Request('GET', IG_URL + "/markets/" + wheat_epic, headers=headers, params='').prepare()).json()['instrument']['lotSize']
 
-        # UK ticker code > IG epic mapping.
-        ticker_epic = {
-            "UKOIL": brent_epic_uk,
-            "CFDs on Brent Crude Oil": brent_epic_uk,
-            "DE30EUR": dax_epic_uk,
-            "DAX": dax_epic_uk,
-            "WHTUSD": wheat_epic_uk,
-            "WHEATUSD": wheat_epic_uk}
+        else:
+            print("Error: Webhook ticker code not recognised.")
+            return {
+                'statusCode': 400,
+                'body': json.dumps("Webhook ticker code not recognised.")}
 
-        print(webhook_signal)
+        # Ticker code: EPIC code mapping.
+        ticker_epic_map = {
+            "UKOIL": brent_epic,
+            "CFDs on Brent Crude Oil": brent_epic,
+            "DE30EUR": dax_epic,
+            "DAX": dax_epic,
+            "WHTUSD": wheat_epic,
+            "WHEATUSD": wheat_epic}
 
-        # Place entry, stop, and take profit orders.
+        print("Brent", ticker_epic_map['UKOIL'], brent_expiry, brent_lotsize)
+        print("DAX", ticker_epic_map['DAX'], dax_expiry, dax_lotsize)
+        print("Chicago Wheat", ticker_epic_map['WHEATUSD'], wheat_expiry, wheat_lotsize)
+
+        # Place entry, then place stop and take profit orders  using the returned entry price.
 
     else:
         print("Webhook signal token error")
@@ -149,13 +148,16 @@ def lambda_handler(event, context):
             'body': json.dumps("Webhook signal token error")}
 
 
-event = {"body": '{"text": "Test", "token": "7f3c4d9a-9ac3-4819-b997-b8ee294d5a42"}'}
+event = {"body": '{"ticker": "UKOIL", "exchange": "TVC", "open": 42.42,  "close": 42.57, "high": 42.68, "low": 42.34, "volume": 806, "time": "2019-08-27T09:56:00Z", "text": "", "token": "7f3c4d9a-9ac3-4819-b997-b8ee294d5a42"}'}
 
-# Format this with real values for testing
-# {"ticker": {{ticker}}, "exchange": {{exchange}}, "open": {{open}},  "close": {{close}}, "high": {{high}}, "low": {{low}}, "volume": {{volume}}, "time": {{time}}, "text": "", "token": "7f3c4d9a-9ac3-4819-b997-b8ee294d5a42"}
 
 # Paste this into webhook
 # {"ticker": {{ticker}}, "exchange": {{exchange}}, "open": {{open}},  "close": {{close}}, "high": {{high}}, "low": {{low}}, "volume": {{volume}}, "time": {{time}}, "text": "", "token": "7f3c4d9a-9ac3-4819-b997-b8ee294d5a42"}
 
 
 lambda_handler(event, context=None)
+
+
+# UKOIL - 210m
+# WHEATUSD - 120m
+# DE30EUR - 60m
